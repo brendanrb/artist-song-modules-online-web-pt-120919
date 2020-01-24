1cf0cd82ad802188cc7d1aa144169d76b1db1636
Brendan R Borgers <brendanborgers@gmail.com>
2020-01-24 00:20:00 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,9 +1,6 @@
 module Findable
-  
+  module ClassMethods
+    
   def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-  
-   def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
+    self.detect{|a| a.name == name}
   end
